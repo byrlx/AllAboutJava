1a923adf672ce72b62b7205bb73514e25eb38b40
luis <xuzhengchaojob@gmail.com>
2016-04-10 14:04:38 +0800

    unifind

--- src/main/java/com/lx/Main.java
+++ src/main/java/com/lx/Main.java
@@ -2,6 +2,7 @@ package com.lx;
 
 import com.lx.Util.Log;
 import com.lx.algorithm.trees.RbTree;
+import com.lx.reflect.FancyLog;
 import com.lx.rxjava.Learn;
 
 public class Main {
@@ -12,6 +13,7 @@ public class Main {
 //        MysqlTester.start();
 //        System.out.println(Thread.currentThread());
         Log.e("main");
-        RbTree.test();
+//        RbTree.test();
+        FancyLog.test();
     }
 }
--- src/main/java/com/lx/algorithm/trees/TreeUtils.java
+++
@@ -1,7 +0,0 @@
-package com.lx.algorithm.trees;
-
-/**
- * Created by Zhengchao Xu on 3/18/16.
- */
-public class TreeUtils {
-}
