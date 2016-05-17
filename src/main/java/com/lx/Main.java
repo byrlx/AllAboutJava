56f6f1969c642673ddc118dd682fdae0bd747796
luis <xuzhengchaojob@gmail.com>
2016-05-17 23:37:58 +0800

    update

--- src/main/java/com/lx/Main.java
+++ src/main/java/com/lx/Main.java
@@ -1,10 +1,13 @@
 package com.lx;
 
+import com.lx.concurrent.CocurrentQ;
+import com.lx.rtti.DoD;
+import com.lx.rtti.ProxyTest;
 import com.lx.util.IoUtils;
 import com.lx.util.StringUtils;
 
 public class Main {
-    public static void main(String[] args) {
+    private void old() {
 //        (new Sort()).start();
 //        (new Learn()).start();
 //        GitParser.test();
@@ -13,6 +16,12 @@ public class Main {
 //        RbTree.test();
 //        FancyLog.test();
 //        StringUtils.test();
-        IoUtils.test();
+//        IoUtils.test();
+//        DoD dod = ProxyTest.create(DoD.class);
+//        dod.print("hi");
+    }
+
+    public static void main(String[] args) {
+        CocurrentQ.test();
     }
 }
---
+++ src/main/java/com/lx/rtti/ProxyTest.java
@@ -0,0 +1,29 @@
+package com.lx.rtti;
+
+import com.lx.util.Common;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+/**
+ * Created by douhua on 4/13/16.
+ */
+public class ProxyTest {
+    @SuppressWarnings("unchecked")
+    public static <T> T create(final Class<T> service) {
+        return (T) Proxy.newProxyInstance(service.getClassLoader(), new Class[]{service}, new InvocationHandler() {
+            @Override
+            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                String clsName = service.getSimpleName();
+                String mthdName = method.getName();
+                System.out.printf("ClassName:%s, MethodName:%s, Args:", clsName, mthdName);
+                for (Object arg : args) {
+                    System.out.printf("%s", arg);
+                }
+                System.out.println();
+                return null;
+            }
+        });
+    }
+}
